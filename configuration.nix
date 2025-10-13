64d4159d704a365ed45f9e466c80f73627ad73ef
Brian D. Wendt <bws428@pm.me>
2025-10-13 18:47:58 -0400

    Initial commit

---
+++ configuration.nix
@@ -0,0 +1,151 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ 
+      ./hardware-configuration.nix
+      ./nvidia.nix
+    ];
+
+  # Bootloader (systemd)
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Use LTS kernel (so that NVIDIA drivers will build)
+  # https://nixos.wiki/wiki/Linux_kernel
+  boot.kernelPackages = pkgs.linuxPackages;
+
+  # Define your hostname
+  networking.hostName = "ghost";
+  
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+  # networking.wireless.enable = true; # one or the other, NOT both!
+
+  # Set your time zone.
+  time.timeZone = "America/New_York";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable GNOME desktop environment
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  # Enable Hyprland tiling window manager
+  programs.hyprland.enable = true;
+
+  # Suggest Electron apps use Wayland
+  # environment.sessionVariables.NIXOS_OZONE_WL = "1";
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.bws428 = {
+    isNormalUser = true;
+    description = "Brian W.";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+    #  e.g., thunderbird # Packages listed here are ONLY available to this user account!
+    ];
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # Install Firefox web browser
+  programs.firefox.enable = true;
+
+  # Install Steam + necessary firewall settings
+  # https://nixos.wiki/wiki/Steam
+  programs.steam.enable = true;
+  programs.steam = {
+    remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = true;
+    localNetworkGameTransfers.openFirewall = true;
+  };
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    git gh curl wget obsidian signal-desktop kitty ghostty starship zsh btop microfetch zed-editor helix neovim
+    eza zoxide zsh-autosuggestions zsh-syntax-highlighting nvtopPackages.nvidia localsend wofi
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "25.05"; # Did you read the comment?
+
+}
---
+++ nvidia.nix
@@ -0,0 +1,57 @@
+{ config, pkgs, ... }:
+
+{
+  # Blacklist AMD GPU driver
+  boot.blacklistedKernelModules = [ "amdgpu" ];
+  
+  # Nvidia-specific kernel parameters
+  boot.kernelParams = [ 
+    "nvidia-drm.modeset=1"
+    "nvidia-drm.fbdev=1"
+    # RTX 5080-specific parameters for suspend/resume
+    "nvidia.NVreg_PreserveVideoMemoryAllocations=1"
+    "nvidia.NVreg_TemporaryFilePath=/var/tmp"
+  ];
+  
+  # Load Nvidia modules in initrd
+  boot.initrd.kernelModules = [ 
+    "nvidia" 
+    "nvidia_modeset" 
+    "nvidia_uvm" 
+    "nvidia_drm" 
+  ];
+  
+  # Enable Nvidia drivers
+  services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.graphics.enable = true;
+  
+  # Nvidia driver configuration
+  hardware.nvidia = {
+    open = true;  # Required for RTX 5080 (Blackwell architecture)
+    modesetting.enable = true;
+    nvidiaSettings = true;
+    
+    # Power management settings
+    powerManagement.enable = true;
+    powerManagement.finegrained = false;
+    
+    # Use production driver (565 series)
+    package = config.boot.kernelPackages.nvidiaPackages.production;
+  };
+  
+  # Create temp directory for Nvidia driver
+  systemd.tmpfiles.rules = [
+    "d /var/tmp 1777 root root -"
+  ];
+
+  # Gnome/Wayland specific settings for Nvidia
+  services.xserver.displayManager.gdm.wayland = true;
+
+  # Add environment variables for Nvidia + Wayland
+  environment.sessionVariables = {
+    LIBVA_DRIVER_NAME = "nvidia";
+    GBM_BACKEND = "nvidia-drm";
+    __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+    WLR_NO_HARDWARE_CURSORS = "1";
+  };
+}
