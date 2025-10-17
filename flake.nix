7e395cc894a83f5f33a33b0d52c7c72a6d5a111c
Brian D. Wendt <627118+bws428@users.noreply.github.com>
2025-10-17 15:39:35 -0400

    Added HM backup to avoid build error

--- flake.nix
+++ flake.nix
@@ -21,6 +21,7 @@
         home-manager.nixosModules.home-manager {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
+            home-manager.backupFileExtension = "backup";
             home-manager.users.bws428 = import ./home.nix;
         }
       ];
