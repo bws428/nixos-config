3b6b0d3cc1b8af59503f32f7576103497f7ecf71
Brian D. Wendt <627118+bws428@users.noreply.github.com>
2025-10-17 15:18:16 -0400

    REALLY add Home Manager this time

--- flake.nix
+++ flake.nix
@@ -4,13 +4,25 @@
   inputs = {
     # NixOS official package source, using nixos-20.05 branch
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    # Add Home Manager
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, ...}@inputs: {
+  outputs = @inputs{ self, nixpkgs, home-manager, ... }: {
     # NixOS system configuration - `ghost` is the hostname
     nixosConfigurations.ghost = nixpkgs.lib.nixosSystem {
       modules = [
+        # Legacy NixOS config file
         ./configuration.nix
+        # Home Manager module
+        home-manager.nixosModules.home-manager {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.bws428 = import ./home.nix;
+        }
       ];
     };
   };
