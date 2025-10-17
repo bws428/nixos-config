fbd492f6ad83d36b50304d3c2c8df7e00a936b9f
Brian D. Wendt <627118+bws428@users.noreply.github.com>
2025-10-16 21:11:28 -0400

    Remove plymouth

--- flake.nix
+++ flake.nix
@@ -4,18 +4,14 @@
   inputs = {
     # NixOS official package source, using nixos-20.05 branch
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
-    # NixOS plymouth boot splash
-    nixos-plymouth.url = "github:BeatLink/nixos-plymouth";
   };
 
-  outputs = { self, nixpkgs, nixos-plymouth, ...}@inputs: {
+  outputs = { self, nixpkgs, ...}@inputs: {
     # NixOS system configuration - `ghost` is the hostname
     nixosConfigurations.ghost = nixpkgs.lib.nixosSystem {
       modules = [
         ./configuration.nix
-        nixos-plymouth.nixosModules.default
       ];
-      system = "x86_64-linux";
     };
   };
 }
