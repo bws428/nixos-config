2b34e8ab49e5ea8846cfc784958a5aa17ba82219
Brian D. Wendt <627118+bws428@users.noreply.github.com>
2025-10-17 15:41:38 -0400

    Removed draw_bold_text from Alacritty config

--- home.nix
+++ home.nix
@@ -115,12 +115,10 @@
   # alacritty - a cross-platform, GPU-accelerated terminal emulator
   programs.alacritty = {
     enable = true;
-    # custom settings
     settings = {
       env.TERM = "xterm-256color";
       font = {
         size = 14;
-        draw_bold_text_with_bright_colors = true;
         normal = {
           family = "MesloLGS Nerd Font Mono";
           style = "Regular";
