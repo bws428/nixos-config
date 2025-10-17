e250db80b1574e129fb24d573e58082635a2151f
Brian D. Wendt <627118+bws428@users.noreply.github.com>
2025-10-16 21:02:20 -0400

    Move plymouth to flake.nix

--- flake.nix
+++ flake.nix
@@ -4,14 +4,18 @@
   inputs = {
     # NixOS official package source, using nixos-20.05 branch
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    # NixOS plymouth boot splash
+    nixos-plymouth.url = "github:BeatLink/nixos-plymouth";
   };
 
-  outputs = { self, nixpkgs, ...}@inputs: {
+  outputs = { self, nixpkgs, nixos-plymouth ...}@inputs: {
     # NixOS system configuration - `ghost` is the hostname
     nixosConfigurations.ghost = nixpkgs.lib.nixosSystem {
       modules = [
         ./configuration.nix
+        nixos-plymouth.nixosModules.default
       ];
+      system = "x86_64-linux";
     };
   };
 }
