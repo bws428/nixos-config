51224e9a7b4eb576e7e9578ca17f0fd69f33f3d6
Brian D. Wendt <627118+bws428@users.noreply.github.com>
2025-10-19 08:50:02 -0400

    Switch to NixOS unstable branch

--- flake.nix
+++ flake.nix
@@ -1,19 +1,25 @@
 {
-  description = "NixOS primary system configuration flake";
+  description = "NixOS primary system configuration";
 
   inputs = {
-    # NixOS official package source, using nixos-20.05 branch
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    # NixOS official package source, unstable branch
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
     # Add Home Manager
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
     # Add Stylix
-    stylix.url = "github:nix-community/stylix/release-25.05";
+    stylix = {
+      url = "github:nix-community/stylix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     # Add MangoWC
     mangowc = {
-      url = "github:DreamMaoMao/mangowc/release-25.05";
+      url = "github:DreamMaoMao/mangowc";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
@@ -22,8 +28,10 @@
     # NixOS system configuration - `ghost` is the hostname
     nixosConfigurations.ghost = nixpkgs.lib.nixosSystem {
       modules = [
+
         # Legacy NixOS config file
         ./configuration.nix
+
         # Home Manager module
         home-manager.nixosModules.home-manager {
             home-manager.useGlobalPkgs = true;
@@ -31,8 +39,10 @@
             home-manager.backupFileExtension = "backup";
             home-manager.users.bws428 = import ./home/home.nix;
         }
+
         # Stylix
         stylix.nixosModules.stylix
+
         # MangoWC
         mangowc.nixosModules.default
       ];
