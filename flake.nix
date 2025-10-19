d11c8240a7e4ea3548cd946fd05af8e022fdde51
Brian D. Wendt <627118+bws428@users.noreply.github.com>
2025-10-19 11:08:55 -0400

    Still trying to add Mango

--- flake.nix
+++ flake.nix
@@ -28,7 +28,7 @@
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
               home-manager.backupFileExtension = "backup";
-              home-manager.users.bws428 = import ./home/home/nix
+              home-manager.users.bws428 = import ./home/home/nix;
             }
           ];
         };
