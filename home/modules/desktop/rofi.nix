07b1bc9806a7437806abcdc1dd19f8da1973b2e9
Brian D. Wendt <627118+bws428@users.noreply.github.com>
2025-10-20 11:03:53 -0400

    NixOS rebuild

--- home/modules/desktop/rofi.nix
+++ home/modules/desktop/rofi.nix
@@ -3,5 +3,15 @@
 {
   programs.rofi = {
     enable = true;
+    theme = "Arc-Dark";
+
+      extraConfig = {
+        modi = "drun,run,window";
+        show-icons = true;
+        icon-theme = "Papirus-Dark";
+        display-drun = "Applications";
+        display-window = "Windows";
+        drun-display-format = "{name}";
+      };
   };
 }
