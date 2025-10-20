2ec7adaf9a7bb1874c9a9a2791a661e2b998500a
Brian D. Wendt <627118+bws428@users.noreply.github.com>
2025-10-20 10:03:56 -0400

    NixOS rebuild

--- configuration.nix
+++ configuration.nix
@@ -54,7 +54,7 @@
     displayManager.lightdm = {
       enable = true;
     };
-  }
+  };
 
   # Enable the gnome-keyring secrets vault.
   # Will be exposed through DBus to programs willing to store secrets.
