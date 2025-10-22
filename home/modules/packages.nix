518e8a52147a53e305b833de207080ca72a2a69d
Brian D. Wendt <627118+bws428@users.noreply.github.com>
2025-10-22 09:58:24 -0400

    NixOS rebuild

--- home/modules/packages.nix
+++ home/modules/packages.nix
@@ -88,7 +88,7 @@
     spotify # streaming music
     spicetify-cli # spice up spotify
     shotcut # video editor
-    #bambu-studio # 3D printer
+    bambu-studio # 3D printer
     orca-slicer # 3D printer
     
   ];
