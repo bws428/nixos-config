433007c6d907cb9217da17d9546d7372cab25953
Brian D. Wendt <627118+bws428@users.noreply.github.com>
2025-10-24 14:36:53 -0400

    NixOS rebuild

--- nvidia.nix
+++ nvidia.nix
@@ -54,8 +54,6 @@
   # Enable OpenGL
   hardware.graphics = {
     enable = true;
-    #driSupport = true;
-    driSupport32Bit = true; # For 32-bit applications
     extraPackages = with pkgs; [
       vaapiVdpau
       libvdpau-va-gl
