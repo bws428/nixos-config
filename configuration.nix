09895e5f7b8519d8b7abef1cf02acc0bb9abfc6e
Brian D. Wendt <627118+bws428@users.noreply.github.com>
2025-10-26 08:45:34 -0400

    NixOS rebuild

--- configuration.nix
+++ configuration.nix
@@ -39,7 +39,6 @@
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
-
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "en_US.UTF-8";
     LC_IDENTIFICATION = "en_US.UTF-8";
@@ -78,7 +77,7 @@
   # Gnome Display Manager (NixOS 25.11+)
   # https://wiki.nixos.org/wiki/GNOME
   services.displayManager.gdm = {
-    enable = true;
+    enable = false;
     wayland = true;
   };
 
