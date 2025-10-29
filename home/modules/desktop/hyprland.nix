ed6b79a5386b8c1c69299f81d54c74aeb742cf64
Brian D. Wendt <627118+bws428@users.noreply.github.com>
2025-10-29 08:43:50 -0400

    NixOS rebuild

--- home/modules/desktop/hyprland.nix
+++ home/modules/desktop/hyprland.nix
@@ -39,8 +39,97 @@
       "$browser" = "firefox";
       "$notes" = "obsidian";
       "$mainMod" = "SUPER";
+      
+      # Autostart programs
+      exec-once = [
+        "hyprpaper"
+        "hyprpanel"
+        "[workspace 1 silent] $browser"
+        "[workspace 2 silent] $terminal"
+        "[workspace 2 silent] $terminal"
+        "[workspace 3 silent] $signal"
+        "[workspace 3 silent] $notes"
+      ];
 
-      # General settings - colors managed by Stylix
+      # Keybindings with descriptions
+      bindd = [
+        "SUPER, Q, Quit, killactive,"
+        "SUPER, return, Terminal, exec, $terminal"
+        "SUPER, space, Applications, exec, $menu"
+        "SUPER, F, File Manager, exec, $fileManager"
+        "SUPER, S, Signal, exec, $signal"
+        "SUPER, B, Web Browser, exec, $browser"
+        "SUPER, T, Top, exec, $terminal -e btop"
+        "SUPER, O, Obsidian, exec, $notes"
+      ];
+
+      # Standard keybindings
+      bind = [
+        "$mainMod, M, exit,"
+        "$mainMod, V, togglefloating,"
+        "$mainMod, P, pseudo,"
+        "$mainMod, J, togglesplit,"
+
+        # Move focus
+        "$mainMod SHIFT, h, movefocus, l"
+        "$mainMod SHIFT, l, movefocus, r"
+        "$mainMod SHIFT, k, movefocus, u"
+        "$mainMod SHIFT, j, movefocus, d"
+
+        # Switch workspaces
+        "$mainMod, 1, workspace, 1"
+        "$mainMod, 2, workspace, 2"
+        "$mainMod, 3, workspace, 3"
+        "$mainMod, 4, workspace, 4"
+        "$mainMod, 5, workspace, 5"
+        "$mainMod, 6, workspace, 6"
+        "$mainMod, 7, workspace, 7"
+        "$mainMod, 8, workspace, 8"
+        "$mainMod, 9, workspace, 9"
+        "$mainMod, 0, workspace, 10"
+
+        # Move windows to workspaces
+        "$mainMod SHIFT, 1, movetoworkspace, 1"
+        "$mainMod SHIFT, 2, movetoworkspace, 2"
+        "$mainMod SHIFT, 3, movetoworkspace, 3"
+        "$mainMod SHIFT, 4, movetoworkspace, 4"
+        "$mainMod SHIFT, 5, movetoworkspace, 5"
+        "$mainMod SHIFT, 6, movetoworkspace, 6"
+        "$mainMod SHIFT, 7, movetoworkspace, 7"
+        "$mainMod SHIFT, 8, movetoworkspace, 8"
+        "$mainMod SHIFT, 9, movetoworkspace, 9"
+        "$mainMod SHIFT, 0, movetoworkspace, 10"
+
+        # Scroll through workspaces
+        "$mainMod, mouse_down, workspace, e+1"
+        "$mainMod, mouse_up, workspace, e-1"
+      ];
+
+      # Mouse bindings
+      bindm = [
+        "$mainMod, mouse:272, movewindow"
+        "$mainMod, mouse:273, resizewindow"
+      ];
+
+      # Media keys (with repeat)
+      bindel = [
+        ",XF86AudioRaiseVolume, exec, wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"
+        ",XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
+        ",XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+        ",XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
+        ",XF86MonBrightnessUp, exec, brightnessctl -e4 -n2 set 5%+"
+        ",XF86MonBrightnessDown, exec, brightnessctl -e4 -n2 set 5%-"
+      ];
+
+      # Media control keys (no repeat)
+      bindl = [
+        ", XF86AudioNext, exec, playerctl next"
+        ", XF86AudioPause, exec, playerctl play-pause"
+        ", XF86AudioPlay, exec, playerctl play-pause"
+        ", XF86AudioPrev, exec, playerctl previous"
+      ];
+      
+      # General settings
       general = {
         gaps_in = 5;
         gaps_out = 20;
@@ -50,7 +139,7 @@
         layout = "dwindle";
       };
 
-      # Decoration settings - shadow color managed by Stylix
+      # Decoration settings
       decoration = {
         rounding = 10;
         rounding_power = 2;
@@ -140,92 +229,7 @@
         sensitivity = -0.5;
       };
 
-      # Autostart programs
-      exec-once = [
-        "hyprpaper"
-        "ashell"
-        "[workspace 1 silent] $browser"
-        "[workspace 2 silent] $terminal"
-        "[workspace 2 silent] $notes"
-      ];
 
-      # Keybindings with descriptions
-      bindd = [
-        "SUPER, Q, Quit, killactive,"
-        "SUPER, return, Terminal, exec, $terminal"
-        "SUPER, space, Applications, exec, $menu"
-        "SUPER, F, File Manager, exec, $fileManager"
-        "SUPER, S, Signal, exec, $signal"
-        "SUPER, B, Web Browser, exec, $browser"
-        "SUPER, T, Top, exec, $terminal -e btop"
-        "SUPER, O, Obsidian, exec, $notes"
-      ];
-
-      # Standard keybindings
-      bind = [
-        "$mainMod, M, exit,"
-        "$mainMod, V, togglefloating,"
-        "$mainMod, P, pseudo,"
-        "$mainMod, J, togglesplit,"
-
-        # Move focus
-        "$mainMod SHIFT, h, movefocus, l"
-        "$mainMod SHIFT, l, movefocus, r"
-        "$mainMod SHIFT, k, movefocus, u"
-        "$mainMod SHIFT, j, movefocus, d"
-
-        # Switch workspaces
-        "$mainMod, 1, workspace, 1"
-        "$mainMod, 2, workspace, 2"
-        "$mainMod, 3, workspace, 3"
-        "$mainMod, 4, workspace, 4"
-        "$mainMod, 5, workspace, 5"
-        "$mainMod, 6, workspace, 6"
-        "$mainMod, 7, workspace, 7"
-        "$mainMod, 8, workspace, 8"
-        "$mainMod, 9, workspace, 9"
-        "$mainMod, 0, workspace, 10"
-
-        # Move windows to workspaces
-        "$mainMod SHIFT, 1, movetoworkspace, 1"
-        "$mainMod SHIFT, 2, movetoworkspace, 2"
-        "$mainMod SHIFT, 3, movetoworkspace, 3"
-        "$mainMod SHIFT, 4, movetoworkspace, 4"
-        "$mainMod SHIFT, 5, movetoworkspace, 5"
-        "$mainMod SHIFT, 6, movetoworkspace, 6"
-        "$mainMod SHIFT, 7, movetoworkspace, 7"
-        "$mainMod SHIFT, 8, movetoworkspace, 8"
-        "$mainMod SHIFT, 9, movetoworkspace, 9"
-        "$mainMod SHIFT, 0, movetoworkspace, 10"
-
-        # Scroll through workspaces
-        "$mainMod, mouse_down, workspace, e+1"
-        "$mainMod, mouse_up, workspace, e-1"
-      ];
-
-      # Mouse bindings
-      bindm = [
-        "$mainMod, mouse:272, movewindow"
-        "$mainMod, mouse:273, resizewindow"
-      ];
-
-      # Media keys (with repeat)
-      bindel = [
-        ",XF86AudioRaiseVolume, exec, wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"
-        ",XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
-        ",XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
-        ",XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
-        ",XF86MonBrightnessUp, exec, brightnessctl -e4 -n2 set 5%+"
-        ",XF86MonBrightnessDown, exec, brightnessctl -e4 -n2 set 5%-"
-      ];
-
-      # Media control keys (no repeat)
-      bindl = [
-        ", XF86AudioNext, exec, playerctl next"
-        ", XF86AudioPause, exec, playerctl play-pause"
-        ", XF86AudioPlay, exec, playerctl play-pause"
-        ", XF86AudioPrev, exec, playerctl previous"
-      ];
 
       # Window rules
       windowrule = [
