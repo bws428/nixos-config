977280bce7e4a18743b5819e7cf18eab21d1a423
Brian D. Wendt <627118+bws428@users.noreply.github.com>
2025-10-31 10:26:39 -0400

    NixOS rebuild

--- configuration.nix
+++ configuration.nix
@@ -128,9 +128,6 @@
     xwayland.enable = true; # Steam needs this
   };
 
-  # Enable the COSMIC login manager
-  services.displayManager.cosmic-greeter.enable = true;
-
   # Enable the COSMIC desktop environment
   services.desktopManager.cosmic.enable = true;
 
