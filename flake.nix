94dcec9416bd4fcab1e0d43e1b854cf4b69a6d8b
Brian D. Wendt <627118+bws428@users.noreply.github.com>
2025-10-31 10:49:35 -0400

    NixOS rebuild

--- flake.nix
+++ flake.nix
@@ -14,6 +14,22 @@
     # Mango compositor
     mango.url = "github:DreamMaoMao/mango";
 
+    # Dank Material Shell (Mango theme & tools)
+    dgop = {
+      url = "github:AvengeMedia/dgop";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    dms-cli = {
+      url = "github:AvengeMedia/danklinux";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    dankMaterialShell = {
+      url = "github:AvengeMedia/DankMaterialShell";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.dgop.follows = "dgop";
+      inputs.dms-cli.follows = "dms-cli";
+    };
+
   };
 
   outputs = inputs @ { self, nixpkgs, home-manager, mango, ... }: {
