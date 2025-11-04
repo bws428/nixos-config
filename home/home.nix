bc6f667018f05afb2e6e0e693683485652a4a30f
Brian D. Wendt <627118+bws428@users.noreply.github.com>
2025-11-04 10:14:30 -0500

    NixOS rebuild

--- home/home.nix
+++ home/home.nix
@@ -19,7 +19,7 @@
   home.homeDirectory = "/home/bws428";
   home.stateVersion = "25.05";
 
-  X resources for cursor/dpi
+  # X resources for cursor/dpi
   xresources.properties = {
     "Xcursor.size" = 16;
     "Xft.dpi" = 172;
