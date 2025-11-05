bb474f1992f5b2475afd584a616278fa1d00f4a1
Brian D. Wendt <627118+bws428@users.noreply.github.com>
2025-11-05 18:21:04 -0500

    NixOS rebuild

--- home/modules/desktop/dms.nix
+++ home/modules/desktop/dms.nix
@@ -1,6 +1,9 @@
 { pkgs, ... }:
 
 {
+  # Dank Material Shell
+  # https://danklinux.com
+
   programs.dankMaterialShell = {
     enable = true;
 
@@ -9,7 +12,7 @@
     enableSystemMonitoring = true;     # System monitoring widgets (dgop)
     enableClipboard = true;            # Clipboard history manager
     enableVPN = true;                  # VPN management widget
-    enableBrightnessControl = true;    # Backlight/brightness controls
+    enableBrightnessControl = false;   # Backlight/brightness controls
     enableColorPicker = true;          # Color picker tool
     enableDynamicTheming = true;       # Wallpaper-based theming (matugen)
     enableAudioWavelength = true;      # Audio visualizer (cava)
