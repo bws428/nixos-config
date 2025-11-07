dfbf64ce34a5b66e00da2e297535cbda372c127d
Brian D. Wendt <627118+bws428@users.noreply.github.com>
2025-11-07 13:39:30 -0500

    NixOS rebuild

--- flake.nix
+++ flake.nix
@@ -8,23 +8,19 @@
     # Home Manager
     home-manager = {
       url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
     };
 
     # Dank Material Shell
     dgop = {
       url = "github:AvengeMedia/dgop";
-      inputs.nixpkgs.follows = "nixpkgs";
     };
 
     dms-cli = {
       url = "github:AvengeMedia/danklinux";
-      inputs.nixpkgs.follows = "nixpkgs";
     };
 
     dankMaterialShell = {
       url = "github:AvengeMedia/DankMaterialShell";
-      inputs.nixpkgs.follows = "nixpkgs";
       inputs.dgop.follows = "dgop";
       inputs.dms-cli.follows = "dms-cli";
     };
