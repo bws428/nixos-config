2a7e03f579eaa93d26f75ccfc6c95e69c912719a
Brian D. Wendt <627118+bws428@users.noreply.github.com>
2025-11-08 06:24:28 -0500

    NixOS rebuild

--- home.nix
+++ home.nix
@@ -10,6 +10,10 @@
   # introduces backwards incompatible changes.
   home.stateVersion = "25.05";
 
+  # Let Home Manager install and manage itself
+  programs.home-manager.enable = true;
+
+  # Configurations to manage
   imports = [
     ./config/shell.nix
     ./config/helix.nix
