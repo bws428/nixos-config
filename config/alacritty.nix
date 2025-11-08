85e5acd01e2d94216d08edb2dd9ad28cd7260853
Brian D. Wendt <627118+bws428@users.noreply.github.com>
2025-11-08 10:47:37 -0500

    NixOS rebuild

--- config/alacritty.nix
+++ config/alacritty.nix
@@ -5,13 +5,13 @@
     enable = true;
     theme = "catppuccin_macchiato";
     settings = {
-      font.size = 16;
+      font.size = 15;
       font.normal = {
         family = "JetBrainsMono Nerd Font";
         style = "Regular";
       };
       env.TERM = "xterm-256color";
-      window.opacity = 0.90;
+      window.opacity = 0.85;
       window.blur = true;
       scrolling.multiplier = 5;
       selection.save_to_clipboard = true;
