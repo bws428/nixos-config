0183f7294e4cf5acf8079fe36a5bc921c36447b5
Brian D. Wendt <627118+bws428@users.noreply.github.com>
2025-11-09 08:31:25 -0500

    NixOS rebuild

--- config/niri/niri.nix
+++ config/niri/niri.nix
@@ -4,9 +4,6 @@
   # Niri support packages
   home.packages = with pkgs; [
     fuzzel # default app launcher
-    mako # notifications
-    grim # screenshot
-    slurp # screenshot
     xwayland-satellite # xwayland support
   ];
 }
