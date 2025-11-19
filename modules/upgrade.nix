b2fc443c2e086d3e5da7a8fe1d2c9dadf2d19b9d
Brian D. Wendt <627118+bws428@users.noreply.github.com>
2025-11-19 10:25:16 -0500

    NixOS rebuild

--- modules/upgrade.nix
+++ modules/upgrade.nix
@@ -1,13 +1,16 @@
-{ config, pkgs, ... }:
+{ config, pkgs, inputs ... }:
 
 {
   # Automatic system upgrades (with flakes)
   # https://wiki.nixos.org/wiki/Automatic_system_upgrades
   system.autoUpgrade = {
     enable = true;
-    flake = "/home/bws428/.nixos-config#";
-    flags = ["--print-build-logs"];
-    dates = "weekly";
+    flake = inputs.self.outPath;
+    flags = [
+      "--print-build-logs"
+    ];
+    dates = "02:00";
+    randomizedDelaySec = "45min";
   };
 
   # Automatic system cleanup
