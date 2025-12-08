8da1e41650b58784744238e9cba7d7ec52a8d7a3
Brian D. Wendt <627118+bws428@users.noreply.github.com>
2025-12-08 11:11:24 -0500

    NixOS rebuild

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
     };
 
     quickshell = {
-      url = "git+https://git.outfoxxed.me/outfoxxed/quickshell";
+      url = "https://git.outfoxxed.me/quickshell/quickshell/";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
