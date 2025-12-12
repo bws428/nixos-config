6c3a891a4bdb15b03e17fc0bc733b3d2166b62ee
Brian D. Wendt <627118+bws428@users.noreply.github.com>
2025-12-12 07:19:49 -0500

    NixOS rebuild

--- config/niri/niri.nix
+++ config/niri/niri.nix
@@ -3,6 +3,7 @@
 {
   # Niri support packages
   home.packages = with pkgs; [
+    # Most of these aren't needed with DMS working...
     mako # notification daemon
     waybar # desktop menu bar
     swaybg # desktop wallpaper
